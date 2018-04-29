b298aef07f638c0a53bcc384bbc1ff9edfb586f5
trishlstark <trishlstark@gmail.com>
2018-04-29 10:01:24 -0600

    Assignment 6 Complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -5,20 +5,30 @@ class Album extends Component {
   constructor(props){
     super(props);
 
-    const album = albumData.find(album => {
-      return album.slug === this.props.match.params.slug
+    const specificAlbum = albumData.find(a => {
+      return a.slug === this.props.match.params.slug
     });
 
     this.state ={
-      album: album
+      album: specificAlbum
     };
+  }
+
+  formatTime(seconds){
+    //70 seconds = 1:10
+    var minutes = Math.floor(seconds / 60);
+    var s = Math.floor(seconds % 60);
+    if(s < 10){
+      s = "0" + s;
+    }
+    return minutes +":" + s;
   }
 
   render () {
     return (
       <section className="album">
         <section id="album-info">
-        <img id="album-cover-art" src={this.state.album.albumCover} />
+        <img id="album-cover-art" src={this.state.album.albumCover} alt="" />
           <div className="album-details">
             <h1 id="album-titls">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
@@ -32,6 +42,14 @@ class Album extends Component {
               <col id="song-duration-column"/>
             </colgroup>
             <tbody>
+              {this.state.album.songs.map( (song, index) =>
+                  <tr className="song" key={index}>
+                  <td className="song-number">{index +1}</td>
+                  <td className="song-title">{song.title}</td>
+                  <td className="song-duration">{this.formatTime(song.duration)}</td>
+                  </tr>
+
+                )}
             </tbody>
           </table>
       </section>
